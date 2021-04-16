7dfbbafa42dee5fc8c0c5b230b8cfb2cd6c2e1ee
gb-0001 <gb-0001@outlook.fr>
2021-04-16 12:01:38 +0200

    ex6 pair_impair.py

---
+++ exercice6_pair_impair.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+def pair(nbre):
+    calcule=nbre % 2
+    if calcule == 0:
+        return True
+
+def impair(nbre):
+    calcule=nbre % 2
+    if calcule != 0:
+        return True
+
+nombre = int(input("Saisir une valeur:"))
+
+retourpair=pair(nombre)
+retourimpair=impair(nombre)
+
+if retourpair:
+    print("La fonction pair retourne True pour la valeur {}".format(nombre))
+elif retourimpair:
+    print("La fonction impair retourne True pour la valeur {}".format(nombre))
