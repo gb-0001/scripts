3794f5bc304f7c4a0db12643e40b49ab7ea3abd5
gb-0001 <gb-0001@outlook.fr>
2021-04-16 12:00:06 +0200

    ex6 mini_maxi.py

---
+++ exercice6_mini_maxi.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+def mini(a,b):
+    liste=[a,b]
+    mini1= min(liste)
+    return mini1
+
+def maxi(a,b):
+    liste=[a,b]
+    maxi1= max(liste)
+    return maxi1
+
+
+a = int(input("Saisir une valeur a:"))
+
+b = int(input("Saisir une valeur b:"))
+
+
+
+min1=mini(a,b)
+print("La valeur la plus petite :", min1)
+max1=maxi(a,b)
+print("La valeur la plus grande :", max1)
+
+
