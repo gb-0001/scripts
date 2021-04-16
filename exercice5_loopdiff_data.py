3f36956b9efa2afd8a009b9a3549cf63ed78d30f
gb-0001 <gb-0001@outlook.fr>
2021-04-16 10:19:45 +0200

    Boucler sur différentes données

---
+++ exercice5_loopdiff_data.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+for val in range(1,42):
+    print("Je dois refaire les exercices")
+
+n=10
+while not(n == 0):
+    if n == 1:
+        print("C'est dans % dan je m'en irai j'entends le loup le renard chanter" %n)
+    else:
+        print("C'est dans % dans je m'en irai j'entends le loup le renard chanter" %n)
+    n = n - 1
